d85d3b92c5c7ffcb3ef9ceccd4ff66f178c22c7f
Rob Blumberg <robertblumberg97@gmail.com>
2019-11-28 17:13:03 -0800

    Added second plot in dashboard

--- src/app.py
+++ src/app.py
@@ -5,9 +5,9 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import altair as alt
 import vega_datasets
-from make_plot import make_titanic_plot
+from make_plot import make_titanic_plot, make_class_plot
 
-app = dash.Dash(__name__, assets_folder='assets')
+app = dash.Dash(__name__, assets_folder='assets', external_stylesheets=[dbc.themes.CERULEAN])
 server = app.server
 
 app.title = 'Fate of Titanic Passengers by Location'
@@ -19,10 +19,6 @@ jumbotron = dbc.Jumbotron(
                 html.Img(src='https://www.thoughtco.com/thmb/JAh1c6CpoPyyuPo3H-kEi0ZocZ0=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-titanic-HK4695-001-57d8674f3df78c58339ac2fd.jpg', 
                       width='500px'),
                 html.H1("Fate of Titanic Passengers by Location", className="display-3"),
-                #html.P(
-                #    "Explore Titanic survivors by cabin location!",
-                #    className="lead",
-                #),
             ],
             fluid=True,
         )
@@ -30,33 +26,49 @@ jumbotron = dbc.Jumbotron(
     fluid=True,
 )
 
-content = dbc.Container([
-    dbc.Row(
+row1 = dbc.Row(
                 [dbc.Col(
                     html.Iframe(
                         sandbox='allow-scripts',
                         id='plot',
                         height='550',
-                        width='2000',
-                        style={'border-width': '5px'},
-                        ),width='6',),
+                        width='1600',
+                        style={'border-width': '2px'},
+                        ), width=10,),
 
-                    html.H3("Choose a deck level to explore:", className="display-3"),
-
-                    dbc.Col(        
-                        dcc.Dropdown(
-                            id='dd-chart',
-                            options=[
-                                {'label': 'Deck A', 'value': 'A'},
-                                {'label': 'Deck B', 'value': 'B'}
+                dbc.Col(        
+                    html.Img(src='https://www.thoughtco.com/thmb/JAh1c6CpoPyyuPo3H-kEi0ZocZ0=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-titanic-HK4695-001-57d8674f3df78c58339ac2fd.jpg', 
+                      width='100x'), width=2
+                )]
+            )
+            
+row2 = dbc.Row(
+                [dbc.Col(        
+                    dcc.Dropdown(
+                        id='dd-chart',
+                        options=[
+                            {'label': 'Deck A', 'value': 'A'},
+                            {'label': 'Deck B', 'value': 'B'}
                         ],
                         value='B', style=dict(width='45%')
-                        ), width=2
-                    )
-                ]
-            )
-    ]
-)
+                        ), width='6'
+                    )])
+
+row3 = dbc.Row(
+                [dbc.Col(
+                    html.Iframe(
+                        sandbox='allow-scripts',
+                        id='plot2',
+                        height='550',
+                        width='1600',
+                        style={'border-width': '2px'},
+                        srcDoc = make_class_plot().to_html()
+                        ), width=6),
+
+                dbc.Col(        
+                    html.Img(src='https://www.thoughtco.com/thmb/JAh1c6CpoPyyuPo3H-kEi0ZocZ0=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-titanic-HK4695-001-57d8674f3df78c58339ac2fd.jpg', 
+                      width='100x'), width=2
+                )]) 
 
 footer = dbc.Container([
             dbc.Row(
@@ -68,7 +80,9 @@ footer = dbc.Container([
 )
 
 app.layout = html.Div([jumbotron,
-                       content,
+                       row1,
+                       row2,
+                       row3, 
                        footer])
 
 @app.callback(
